7387effb9381a4356c46cdcc147e0131964e5b06
Sindre Bøyum <boyum@users.noreply.github.com>
2023-01-20 13:25:32 +0100

    feat: add l10n support (#6)

---
+++ src/h5p/h5p.utils.ts
@@ -0,0 +1,11 @@
+import { H5PEditor } from "h5p-utils";
+import type { libraryStrings } from "../../language/en.json";
+import library from "../../library.json";
+
+export const t: (
+  key: keyof typeof libraryStrings,
+  vars?: Record<string, string> | undefined,
+) => string = H5PEditor.t.bind(
+  null,
+  library.machineName as `H5PEditor.${string}`,
+);

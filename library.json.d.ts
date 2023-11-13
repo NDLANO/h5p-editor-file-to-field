9465732eef61ef20eddbab107e0d2c8820d399a0
Oliver Tacke <o.tacke@posteo.de>
2023-11-13 13:37:14 +0100

    Bump patch version

--- library.json.d.ts
+++ library.json.d.ts
@@ -3,7 +3,7 @@ declare const json: {
   "machineName": "H5PEditor.CSVToText",
   "majorVersion": 1,
   "minorVersion": 0,
-  "patchVersion": 14,
+  "patchVersion": 15,
   "runnable": 0,
   "preloadedJs": [
     {

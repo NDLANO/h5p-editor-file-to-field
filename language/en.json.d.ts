3fa5d393442471fd82e544d35eea8eabb1a3d591
Oliver Tacke <o.tacke@posteo.de>
2024-02-26 19:03:47 +0100

    Revert "Delete language/en.json.d.ts"
    This reverts commit 0d795ed6f764a3b8d42aae0d5f159511e26b8689.

---
+++ language/en.json.d.ts
@@ -0,0 +1,8 @@
+declare const json: {
+  "libraryStrings": {
+    "importCSVFileButtonLabel": "Import CSV file",
+    "wordCount_singular": ":count word",
+    "wordCount_plural": ":count words"
+  }
+}
+export default json;

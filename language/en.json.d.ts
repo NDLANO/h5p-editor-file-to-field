0f26eb5c729658246e33b9d9ee3ca483c83fd9f6
Oliver Tacke <o.tacke@posteo.de>
2024-05-10 12:32:26 +0200

    Remove .d.ts template from language folder

--- language/en.json.d.ts
+++
@@ -1,8 +0,0 @@
-declare const json: {
-  "libraryStrings": {
-    "importCSVFileButtonLabel": "Import CSV file",
-    "wordCount_singular": ":count word",
-    "wordCount_plural": ":count words"
-  }
-}
-export default json;
--- src/h5p/h5p.utils.ts
+++ src/h5p/h5p.utils.ts
@@ -1,4 +1,4 @@
-import type { TranslationParams, TranslationHasParams } from 'h5p-types';
+import type { TranslationParams } from 'h5p-types';
 import { H5PEditor } from 'h5p-utils';
 import enTranslation from '../../language/en.json';
 import library from '../../library.json';
@@ -10,9 +10,7 @@ type Translation<T extends TranslationKey> = Translations[T];
 
 export const t: <T extends TranslationKey>(
   key: T,
-  vars?: TranslationHasParams<Translation<T>> extends true
-    ? TranslationParams<Translation<T>>
-    : undefined,
+  vars?: TranslationParams<Translation<T>>
 ) => string = H5PEditor.t.bind(
   null,
   library.machineName as `H5PEditor.${string}`,
